79c50b7695d74c6e16fdef245d444c2fedc6309b
JoeMitchell19 <mitchell.jo@sky.com>
2018-11-13 14:24:30 +0000

    Second Commit

--- src/main/java/com/qa/application/App.java
+++ src/main/java/com/qa/application/App.java
@@ -4,7 +4,22 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World to the Standard out");
-
+		Service inputData = new Service();
+		inputData.addNewAccount("Joe","Mitchell",1);
+		inputData.addNewAccount("Brad","Hudson-Grant",2);
+		inputData.addNewAccount("Kayode","Zollner",3);
+		inputData.addNewAccount("Herve","Malila",4);
+		inputData.addNewAccount("Ross","Lawrence",5);
+		inputData.addNewAccount("John","Gordon",6);
+		
+		Account accountSearchedFor = inputData.findAccount(3);
+		App ap = new App();
+		ap.printInfo(accountSearchedFor);
 	}
+	public void printInfo(Account person) {
+		System.out.println("First Name: " +person.getFirstName());
+		System.out.println("Last Name: "+person.getLastName());
+		System.out.println("Account Number: " +person.getAccountNumber());
 
+}
 }
